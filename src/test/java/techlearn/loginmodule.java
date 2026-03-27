4d7268325bbbf115e9c912d552e85a820a3e5a2b
kalavathi900-ops <kalavathi900@gmail.com>
2026-03-27 23:57:54 +0530

    Framework first commit

---
+++ src/test/java/techlearn/loginmodule.java
@@ -0,0 +1,16 @@
+package techlearn;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+public class loginmodule {
+	WebDriver driver;
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
